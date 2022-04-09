ff440f0dc432fae9124ae85eb47180e2a8f8894d
Mateusz Krystkowicz <krissie515@gmail.com>
2022-04-09 19:17:23 +0200

    02.1 final version

---
+++ 02/src/data.js
@@ -0,0 +1,16 @@
+export const data = [
+  {
+    name: "Adam",
+    age: 22,
+  },
+  {
+    name: "Roman",
+    age: 18,
+  },
+  {
+    name: "Grzegorz",
+    age: 24,
+  },
+];
+
+// export default data;
---
+++ 02/src/index.js
@@ -0,0 +1,7 @@
+import { data } from "./data.js";
+import { combineAge, renderAge } from "./helpers.js";
+
+console.log(combineAge(data));
+const container = document.querySelector("#age");
+
+renderAge(container, data);
