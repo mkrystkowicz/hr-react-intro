f07a58d3baa9ea8eb38035fcd9c76bd3f39edaab
Mateusz Krystkowicz <krissie515@gmail.com>
2022-04-09 17:50:09 +0200

    01.1 final version

--- 01/index.js
+++ 01/index.js
@@ -1,1 +1,17 @@
-'use strict';
+"use strict";
+
+const Parahraph = () => {
+  return React.createElement(
+    "p",
+    {
+      style: { backgroundColor: "blue", fontSize: "bold" },
+      id: "first-paragraph",
+      className: "paragraph",
+    },
+    "Lorem ipsum"
+  );
+};
+
+const appContainer = document.querySelector("#app");
+
+ReactDOM.render(React.createElement(Parahraph), appContainer);
